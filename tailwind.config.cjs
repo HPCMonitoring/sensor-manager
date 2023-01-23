d9b4fe514039f2b9dfed09002bd80cc2bb97f749
phucvinh57 <npvinh0507@gmail.com>
2023-01-23 09:34:57 +0700

    init project

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
