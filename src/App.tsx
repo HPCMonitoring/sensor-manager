7fedc0270e7f2eb5f7c10bcfe730bd5a9abda65c
phucvinh57 <npvinh0507@gmail.com>
2023-02-20 17:30:19 +0700

    fix toggle theme error

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,19 @@ import { SidebarWidth } from "@constants";
 import { Route, Routes } from "react-router-dom";
 import { AppSidebar } from "@components";
 import { HelloWorld } from "@pages";
-import { Flowbite } from "flowbite-react";
 import { useDarkTheme } from "@states";
-import { useCallback } from "react";
+import { useEffect } from "react";
 
 export default function App() {
   const darkTheme = useDarkTheme((state) => state.dark);
 
-  const DarkThemeRenderer = useCallback(() => {
-    return <Flowbite theme={{ dark: darkTheme }}>{null}</Flowbite>;
+  useEffect(() => {
+    console.log(darkTheme);
+    document.getElementById("App")?.classList.toggle("dark");
   }, [darkTheme]);
 
   return (
-    <div>
-      <DarkThemeRenderer />
-
+    <div id='App'>
       <div className='fixed top-0 left-0 h-screen'>
         <AppSidebar />
       </div>
