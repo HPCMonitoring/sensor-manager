e5b9431eb848e997541d6bbfc3aa1d48c720f22a
phucvinh57 <npvinh0507@gmail.com>
2023-02-20 15:00:13 +0700

    fit sidebar to page layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import { Flowbite } from "flowbite-react";
 export default function App() {
   return (
     <Flowbite theme={{ dark: true }}>
-      <AppSidebar />
+      <div className='fixed top-0 left-0 h-screen'>
+        <AppSidebar />
+      </div>
+
       <div id='main' style={{ marginLeft: `${SidebarWidth.EXPAND}px` }}>
         <Routes>
           <Route path='/' element={<HelloWorld />} />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,45 +1,38 @@
 import { Sidebar } from "flowbite-react";
-import {
-  ArrowLeftOnRectangleIcon,
-  ChartPieIcon,
-  InboxIcon,
-  ShoppingBagIcon,
-  Squares2X2Icon,
-  UserIcon,
-  UserPlusIcon
-} from "@heroicons/react/24/solid";
+import { ArrowRightOnRectangleIcon, BellAlertIcon, Squares2X2Icon, WrenchScrewdriverIcon } from "@heroicons/react/24/solid";
 
 export function AppSidebar() {
   return (
-    <Sidebar aria-label='Default sidebar example'>
-      <Sidebar.Logo href='#' img='vite.svg' imgAlt='Logo'>
-        BKTracker
-      </Sidebar.Logo>
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href='#' icon={ChartPieIcon}>
-            Dashboard
-          </Sidebar.Item>
-          <Sidebar.Item href='#' icon={Squares2X2Icon} label='Pro' labelColor='alternative'>
-            Kanban
-          </Sidebar.Item>
-          <Sidebar.Item href='#' icon={InboxIcon} label='3'>
-            Inbox
-          </Sidebar.Item>
-          <Sidebar.Item href='#' icon={UserIcon}>
-            Users
-          </Sidebar.Item>
-          <Sidebar.Item href='#' icon={ShoppingBagIcon}>
-            Products
-          </Sidebar.Item>
-          <Sidebar.Item href='#' icon={ArrowLeftOnRectangleIcon}>
-            Sign In
-          </Sidebar.Item>
-          <Sidebar.Item href='#' icon={UserPlusIcon}>
-            Sign Up
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
+    <Sidebar aria-label='Default sidebar example' className='fb-sidebar w-64'>
+      <div className='flex flex-col h-full'>
+        <div className='flex-none'>
+          <Sidebar.Logo href='#' img='vite.svg' imgAlt='Logo'>
+            BKTracker
+          </Sidebar.Logo>
+        </div>
+
+        <Sidebar.Items className='flex flex-col justify-between h-full'>
+          <Sidebar.ItemGroup>
+            <Sidebar.Collapse icon={Squares2X2Icon} label='Clusters'>
+              <Sidebar.Item href='#'> BK HPC Laboratory </Sidebar.Item>
+              <Sidebar.Item href='#'> UIT Cluster </Sidebar.Item>
+              <Sidebar.Item href='#'> TickLab cloud </Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Item href='#' icon={BellAlertIcon} label='3'>
+              Notifications
+            </Sidebar.Item>
+            <Sidebar.Item href='#' icon={WrenchScrewdriverIcon}>
+              Setting
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+
+          <Sidebar.ItemGroup>
+            <Sidebar.Item href='#' icon={ArrowRightOnRectangleIcon}>
+              Sign out
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </div>
     </Sidebar>
   );
 }
