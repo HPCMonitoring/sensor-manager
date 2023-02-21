a06ecdb053b98ce4ccd8cd72f40367380a1992c8
phucvinh57 <npvinh0507@gmail.com>
2023-02-22 03:09:25 +0700

    add summarize bar

--- src/states/cluster.ts
+++ src/states/cluster.ts
@@ -19,22 +19,22 @@ export const useClustersStore = create<Clusters>()(
               {
                 id: "1915940",
                 name: "BK HPC Laboratory",
-                numOfSensors: 10,
-                numOfActiveSensors: 8,
+                numOfSensors: 7,
+                numOfActiveSensors: 5,
                 remarks: null
               },
               {
                 id: "1456984",
                 name: "TickLab cloud",
-                numOfSensors: 10,
-                numOfActiveSensors: 8,
+                numOfSensors: 4,
+                numOfActiveSensors: 3,
                 remarks: null
               },
               {
                 id: "1433976",
                 name: "UIT Cluster",
-                numOfSensors: 10,
-                numOfActiveSensors: 8,
+                numOfSensors: 6,
+                numOfActiveSensors: 5,
                 remarks: null
               }
             ]
