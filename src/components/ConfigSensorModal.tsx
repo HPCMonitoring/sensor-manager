e75c2afeecff9ee51c1085683636398da1bab6fe
phucvinh57 <npvinh0507@gmail.com>
2023-02-25 16:51:33 +0700

    fuck css

--- src/components/ConfigSensorModal.tsx
+++ src/components/ConfigSensorModal.tsx
@@ -1,7 +1,8 @@
 import { mockSensorDetailInfo, SensorFormField } from "@constants";
-import { SquaresPlusIcon } from "@heroicons/react/24/outline";
-import { useConfigSensorModalStore, useDarkThemeStore } from "@states";
-import { Button, Card, Label, Modal, Textarea, TextInput, Tooltip } from "flowbite-react";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+import { useConfigSensorModalStore } from "@states";
+import { Button, Card, Label, Modal, Textarea, TextInput } from "flowbite-react";
+import { useState } from "react";
 
 function SystemInfo(props: { attr: string; value: string }) {
   return (
@@ -15,67 +16,72 @@ function SystemInfo(props: { attr: string; value: string }) {
 export function ConfigSensorModal() {
   const isOpenConfigSensorModal = useConfigSensorModalStore((state) => state.isOpen);
   const closeConfigSensorModal = useConfigSensorModalStore((state) => state.close);
-  const darkTheme = useDarkThemeStore((state) => state.dark);
+
+  const [isOpenTopicConfig, setIsOpenTopicConfig] = useState(false);
 
   return (
-    <Modal show={isOpenConfigSensorModal} onClose={closeConfigSensorModal} size='md' dismissible position={"top-center"}>
+    <Modal
+      show={isOpenConfigSensorModal}
+      onClose={closeConfigSensorModal}
+      size={isOpenTopicConfig ? "6xl" : "md"}
+      dismissible
+      position={"top-center"}
+    >
       <Modal.Body>
-        <Label htmlFor={SensorFormField.SYSTEM_INFO} value='SYSTEM INFO' className='mb-2' />
-        <Card className='mb-4 mt-1 p-0' id={SensorFormField.SYSTEM_INFO}>
-          {Object.entries(mockSensorDetailInfo).map((entry) => (
-            <SystemInfo
-              key={entry[0]}
-              attr={entry[0]
-                .replace(/([A-Z])/g, " $1")
-                // uppercase the first character
-                .replace(/^./, function (str) {
-                  return str.toUpperCase();
-                })}
-              value={entry[1]}
-            />
-          ))}
-        </Card>
         <form
+          className='w-full'
           onSubmit={(e) => {
             e.preventDefault();
           }}
         >
-          <div className='mb-4 block'>
-            <Label htmlFor={SensorFormField.NAME} value='Sensor name*' className='mb-2' />
-            <TextInput id={SensorFormField.NAME} type='text' required={true} shadow placeholder='Insert cluster name ...' />
-          </div>
+          <div className='grid grid-cols-12 grid-flow-col border-b-2 dark:border-gray-600 mb-4'>
+            <div className={`${isOpenTopicConfig ? "col-span-5" : "col-span-12"}`}>
+              <Label htmlFor={SensorFormField.SYSTEM_INFO} value='SYSTEM INFO' className='mb-2' />
+              <Card className='mb-4 mt-1 p-0' id={SensorFormField.SYSTEM_INFO}>
+                {Object.entries(mockSensorDetailInfo).map((entry) => (
+                  <SystemInfo
+                    key={entry[0]}
+                    attr={entry[0].replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+                      return str.toUpperCase();
+                    })}
+                    value={entry[1]}
+                  />
+                ))}
+              </Card>
 
-          <div className='mb-4 block'>
-            <Label htmlFor={SensorFormField.REMARKS} value='Remarks' />
-            <Textarea id={SensorFormField.REMARKS} placeholder='Insert remarks ...' rows={2} shadow />
-          </div>
+              <div className='mb-4 block'>
+                <Label htmlFor={SensorFormField.NAME} value='Sensor name*' className='mb-2' />
+                <TextInput id={SensorFormField.NAME} type='text' required={true} shadow placeholder='Insert cluster name ...' />
+              </div>
 
-          {/* <hr className="border-gray-400 dark:border-gray-600 mb-4"/> */}
+              <div className='mb-4 block'>
+                <Label htmlFor={SensorFormField.REMARKS} value='Remarks' />
+                <Textarea id={SensorFormField.REMARKS} placeholder='Insert remarks ...' rows={2} shadow />
+              </div>
 
-          <h4 className='text-gray-800 dark:text-gray-100 font-bold mb-4'>Kafka Topics</h4>
-
-          <div className='flex justify-between'>
-            <Tooltip content='Add a Kafka topic' placement='bottom' style={darkTheme ? "light" : "dark"}>
-              <Button
-                gradientMonochrome='info'
+              <div
+                className='hover text-gray-500 dark:text-gray-300 hover:text-blue-700 font-bold outline-none bg-none w-full flex justify-between py-2 px-1 rounded hover:bg-slate-400'
                 onClick={(e) => {
-                  e.stopPropagation();
+                  e.preventDefault();
+                  setIsOpenTopicConfig(!isOpenTopicConfig);
                 }}
-                className='mr-2'
               >
-                <SquaresPlusIcon className='h-4 w-4' />
-              </Button>
-            </Tooltip>
-
-            <div className='flex'>
-              <Button color={"light"} onClick={closeConfigSensorModal}>
-                Cancel
-              </Button>
-              <Button gradientMonochrome='teal' className='ml-2' onClick={closeConfigSensorModal} type='submit'>
-                {" "}
-                Save
-              </Button>
+                <span> See kafka topic settings</span>
+                <ArrowTopRightOnSquareIcon className='h-5 w-5' />
+              </div>
             </div>
+
+            <div className={isOpenTopicConfig ? "" : "hidden"}>asdasdasd</div>
+          </div>
+
+          <div className='flex justify-end'>
+            <Button color={"light"} onClick={closeConfigSensorModal}>
+              Cancel
+            </Button>
+            <Button gradientMonochrome='teal' className='ml-2' onClick={closeConfigSensorModal} type='submit'>
+              {" "}
+              Save
+            </Button>
           </div>
         </form>
       </Modal.Body>
